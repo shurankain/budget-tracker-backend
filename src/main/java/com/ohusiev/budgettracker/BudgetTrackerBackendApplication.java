e5eed2fa03862c6ef047858d7f049bb2daadb69e
Oleksandr Husiev <ohusiev@icloud.com>
2022-08-08 12:48:59 +0200

    missing files added

---
+++ src/main/java/com/ohusiev/budgettracker/BudgetTrackerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ohusiev.budgettracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetTrackerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BudgetTrackerBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ohusiev/budgettracker/BudgetTrackerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohusiev.budgettracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetTrackerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
