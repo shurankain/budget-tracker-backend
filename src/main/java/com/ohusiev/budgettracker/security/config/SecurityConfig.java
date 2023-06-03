dcda2ffe0be703a7d1a6065defbcc9fc2234a06c
Oleksandr Husiev <ohusiev@icloud.com>
2023-06-03 12:45:39 +0200

    spring version updated and security config working with browser added

--- src/main/java/com/ohusiev/budgettracker/security/config/SecurityConfig.java
+++ src/main/java/com/ohusiev/budgettracker/security/config/SecurityConfig.java
@@ -1,12 +1,19 @@
 package com.ohusiev.budgettracker.security.config;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 import com.ohusiev.budgettracker.persistence.repository.UserRepository;
 
@@ -24,14 +31,25 @@ public class SecurityConfig {
     @Bean
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
         http
-                .authorizeExchange()
-                .anyExchange()
-                .authenticated()
-                .and()
-                .httpBasic()
-                .and()
-                .formLogin();
+                .cors(corsSpec -> corsSpec.configurationSource(createCorsConfigSource()))
+                .authorizeExchange(authorizeExchangeSpec -> authorizeExchangeSpec.anyExchange().authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .formLogin(Customizer.withDefaults());
         return http.build();
+    }
+
+    public CorsConfigurationSource createCorsConfigSource() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of("http://localhost:3000/", "http://10.11.211.48:3000/")); // key
+        config.setAllowedHeaders(List.of("*")); // value
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("DELETE");
+
+        source.registerCorsConfiguration("/categories/data/between", config);
+        return source;
     }
 
     @Bean
